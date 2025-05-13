7b5b5aa09198dea598cf42ea4921b2d7cf1172da
Matthieu Fallourd <fallourd.matthieu@gmail.com>
2025-05-13 16:27:09 +0200

    fix: change npm repository

--- build/build.js
+++ build/build.js
@@ -4,10 +4,12 @@ import fs from 'fs';
 async function copyFiles() {
     await fs.copyFile('./package.json', './dist/package.json', () => {});
     await fs.copyFile('./README.md', './dist/README.md', () => {});
-    await fs.copyFile('./bin/tagger.js', './dist/tagger.js', () => {});
+    await fs.copyFile('./bin/tagger.js', './dist/bin/tagger.js', () => {});
 }
 
 fs.mkdir('./dist', () => {});
+fs.mkdir('./dist/bin', () => {});
+
 chalk.white('\n Copy files');
 await copyFiles();
 chalk.green('\n Task completed!');
